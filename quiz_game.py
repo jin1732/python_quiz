04c55423ef0802b2009fef4ad2dfd276b351ad18
손희진 <son1732321732@c6r1s8.codyssey.kr>
2026-08-11 09:58:17 +0900

    Step 3: QuizGame 클래스 완성 및 게임 실행 기능 추가

--- quiz_game.py
+++ quiz_game.py
@@ -18,22 +18,40 @@ class Quiz:
             return False
 
 class QuizGame:
-    def __init__(self):
-        self.quizzes = [] 
+    def __init__(self, questions):
+        self.questions = questions # 여러 개의 퀴즈들을 넘겨받아 저장
         self.score = 0    
 
-    def start_game(self):
-        print("게임을 시작합니다!") 
+    def play(self):
+            print("🍿 천만 영화 퀴즈 게임을 시작합니다! 🍿") 
 
-if __name__ == "__main__":
-    game = QuizGame()
-    game.start_game() 
+         # questions 상자 안에 있는 퀴즈들을 하나씩 꺼내서 진행
+        for quiz in self.question:
+            quiz.display_question() # 문제 보여주기
+            # 사용자에게 키보드로 정답 입력받기 (문자를 숫자로 변환하기 위해 int 사용)
+            user_input = int(input("정답 번호를 입력하세요: "))
+            # 정답 확인하기
+            if quiz.check_answer(user_input):
+                print("✅ 정답입니다!")
+                self.score += 1 # 정답이면 점수 1점 증가
+            else:
+                print(f"❌ 땡! 정답은 {quiz.answer}번입니다.")
 
-    # 보기(options)가 들어간 퀴즈 객체를 하나 만들어봅니다.
-    test_quiz = Quiz("파이썬의 창시자는?", ["1. 스티브 잡스", "2. 귀도 반 로섬", "3. 일론 머스크"], 2)
+    # 게임이 모두 끝나면 최종 점수 출력
+        print(f"\n🎉 게임 종료! 최종 점수: {self.score} / {len(self.questions)}점")
 
-    print("\n--- 임시 데이터 테스트 ---")
-    print("질문:", test_quiz.question)
-    print("보기:", test_quiz.options)
-    print("정답 번호:", test_quiz.answer)
-    print("테스트 성공! 클래스가 완벽하게 작동합니다. 🎉")
+
+# 3. 실제로 게임 작동시키기 (실행 부분)
+# 천만 영화 문제 3개를 만듭니다.
+movie_quizzes = [
+     Quiz("역대 한국 영화 박스오피스 1위는?", ["명량", "극한직업", "신과함께", "국제시장"], 1),
+    Quiz("영화 '극한직업'에서 형사들이 위장 창업한 치킨집 이름은?", ["마약치킨", "수원왕갈비통닭", "대박치킨", "형사통닭"], 2),
+    Quiz("영화 '베테랑'에서 조태오(유아인)의 명대사는?", ["묻고 더블로 가", "내가 빙다리 핫바지로 보이냐", "살려는 드릴게", "어이가 없네"], 4)
+]
+
+# 게임 매니저에게 문제 목록을 넘겨주고 게임 생성
+game = QuizGame(movie_quizzes)
+
+# 게임 시작! (버튼 누르기)
+game.play()
+
