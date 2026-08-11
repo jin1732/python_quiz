724e98ce38fe6474459afbb77a1fbac4f56016bc
jin1732 <son173232@naver.com>
2026-08-11 11:12:04 +0900

    README 설명 추가

--- quiz_game.py
+++ quiz_game.py
@@ -1,12 +1,12 @@
 class Quiz:
-    def __init__(self, question, options, answer):
-        self.question = question
+    def __init__(self, questions, options, answer):
+        self.questions = questions
         self.options = options
         self.answer = answer
 
     # 메서드 1: 퀴즈 출력 (문제와 4개의 보기를 화면에 보여줌)
     def display(self):
-        print(f"\n 문제: {self.question}")
+        print(f"\n 문제: {self.questions}")
         for i in range(4):
             print(f"{i+1}.{self.options[i]}")
 
@@ -23,11 +23,11 @@ class QuizGame:
         self.score = 0    
 
     def play(self):
-            print("🍿 천만 영화 퀴즈 게임을 시작합니다! 🍿") 
+        print("🍿 천만 영화 퀴즈 게임을 시작합니다! 🍿") 
 
          # questions 상자 안에 있는 퀴즈들을 하나씩 꺼내서 진행
-        for quiz in self.question:
-            quiz.display_question() # 문제 보여주기
+        for quiz in self.questions:
+            quiz.display() # 문제 보여주기
             # 사용자에게 키보드로 정답 입력받기 (문자를 숫자로 변환하기 위해 int 사용)
             user_input = int(input("정답 번호를 입력하세요: "))
             # 정답 확인하기
@@ -42,11 +42,9 @@ class QuizGame:
 
 
 # 3. 실제로 게임 작동시키기 (실행 부분)
-# 천만 영화 문제 3개를 만듭니다.
+# 천만 영화 문제 1개를 만듭니다.
 movie_quizzes = [
      Quiz("역대 한국 영화 박스오피스 1위는?", ["명량", "극한직업", "신과함께", "국제시장"], 1),
-    Quiz("영화 '극한직업'에서 형사들이 위장 창업한 치킨집 이름은?", ["마약치킨", "수원왕갈비통닭", "대박치킨", "형사통닭"], 2),
-    Quiz("영화 '베테랑'에서 조태오(유아인)의 명대사는?", ["묻고 더블로 가", "내가 빙다리 핫바지로 보이냐", "살려는 드릴게", "어이가 없네"], 4)
 ]
 
 # 게임 매니저에게 문제 목록을 넘겨주고 게임 생성
